56094618b18d4f598ac75393855052cc2ccb282e
Atul <realatulkamath@gmail.com>
2020-11-09 10:26:04 +0400

    dynamic data now visible on website

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessons=[
+    {
+        id: 1001,
+        title: "<em>Math</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/math.jpg"
+    },
+    {
+        id: 1002,
+        title: "<em>Science</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/science.jpg"
+    },
+    {
+        id: 1003,
+        title: "<em>English</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/english.jpg"
+    },
+    {
+        id: 1004,
+        title: "<em>Computers</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/computer.jpg"
+    },
+    {
+        id: 1005,
+        title: "<em>Physics</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/physics.jpg"
+    },
+    {
+        id: 1006,
+        title: "<em>Chemistry</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/chem.jpg"
+    },
+    {
+        id: 1007,
+        title: "<em>Sales</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/sales.png"
+    },
+    {
+        id: 1008,
+        title: "<em>Architecture</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/architect.png"
+    },
+    {
+        id: 1009,
+        title: "<em>Construction</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/constr.png"
+    },
+    {
+        id: 1010,
+        title: "<em>Philosophy</em>",
+        location: "London",
+        price: 100,
+        spaces: 5,
+        image: "images/phil.jpg"
+    },
+]
